839ae1ebb98e26738f719847fb14e9fe518a98ca
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-10-04 00:26:07 +0300

    Complete Ofxord lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,15 @@
 def oxford_comma(items):
-    return None
+    string = []
+    for item in items:
+        if len(items) == 1:
+            return str(item)
+        elif len(items) == 2:
+            return ' and '.join(items)
+        else:
+            if item != items[-1]:
+                string.append(item)
+            else:
+                string.append(f'and {item}')
+                return str(', '.join(string))
+
+
